b25503eff0507982b3a809ea3861e02982b8b536
Mose Schmiedel <mose.schmiedel@stud.htwk-leipzig.de>
2026-02-25 13:57:55 +0100

    add master
    from moseschmiedel/charging_station-master

---
+++ master/README.md
@@ -0,0 +1,41 @@
+# Charging Station Master Firmware
+
+Firmware for the charging-station master node on an ESP32-S3.  
+It coordinates charging requests from slave nodes using the Dezibot autocharge library.
+
+## Build
+
+```sh
+cd /Volumes/Programming/htwk/charging-station/master
+pio run -e esp32dev
+```
+
+## Flash and Run
+
+```sh
+cd /Volumes/Programming/htwk/charging-station/master
+pio run -e esp32dev -t upload
+```
+
+After upload, the board resets and starts the firmware automatically.
+
+## UART Output (Dezibot)
+
+The firmware prints logs with `Serial.begin(115200)` and uses USB CDC on boot.
+
+1. Connect the ESP32-S3 board via USB.
+2. Find the serial port:
+   ```sh
+   pio device list
+   ```
+3. Open the UART monitor at 115200 baud:
+   ```sh
+   pio device monitor -p /dev/cu.usbmodemXXXX -b 115200
+   ```
+
+Alternative (uses `platformio.ini` monitor settings):
+
+```sh
+cd /Volumes/Programming/htwk/charging-station/master
+pio run -e esp32dev -t monitor
+```
---
+++ master/src/main.cpp
@@ -0,0 +1,40 @@
+#include <Arduino.h>
+#include <Dezibot.h>
+#include <autocharge/Autocharge.hpp>
+
+namespace {
+constexpr uint16_t BEACON_CARRIER_HZ = 10000;
+constexpr uint16_t BEACON_DUTY = 32;
+} // namespace
+
+// put function declarations here:
+void start_chg(Master *master, SlaveData *slave) {
+  Serial.printf("Execute 'start_chg for slave %u'\n", slave->id);
+  master->enjoinCharge(slave);
+}
+void end_chg(Master *master, SlaveData *slave) {
+  Serial.printf("Execute 'end_chg for slave %u'\n", slave->id);
+  master->cancelCharge(slave);
+}
+
+auto chargingSlaves = Fifo<SlaveData *>();
+
+Master master = Master(chargingSlaves, start_chg, end_chg);
+
+void setup() {
+  // put your setup code here, to run once:
+  delay(2000);
+  Serial.begin(115200);
+  Serial.println("+-------------------------+");
+  Serial.println("| Charging Station Master |");
+  Serial.println("+-------------------------+");
+  Serial.println();
+  master.begin();
+  Serial.printf("NodeID '%u'\n", master.communication.getNodeId());
+  master.infraredLight.front.sendFrequency(BEACON_CARRIER_HZ);
+  master.infraredLight.front.setDutyCycle(BEACON_DUTY);
+  Serial.printf("Beacon config: front=%u Hz duty=%u/1023\n", BEACON_CARRIER_HZ,
+                BEACON_DUTY);
+}
+
+void loop() { master.step(); }
